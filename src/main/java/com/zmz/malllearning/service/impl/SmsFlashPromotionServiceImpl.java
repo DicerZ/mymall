42e4d84603ac3c9feff692a36c7f1317db384ba8
DicerZ <438840092@qq.com>
2019-10-29 16:00:14 +0800

    增加限时活动管理

---
+++ src/main/java/com/zmz/malllearning/service/impl/SmsFlashPromotionServiceImpl.java
@@ -0,0 +1,82 @@
+package com.zmz.malllearning.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.zmz.malllearning.mbg.mapper.SmsFlashPromotionMapper;
+import com.zmz.malllearning.mbg.model.SmsFlashPromotion;
+import com.zmz.malllearning.mbg.model.SmsFlashPromotionExample;
+import com.zmz.malllearning.service.SmsFlashPromotionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 限时购活动管理Service实现类
+ * Created by zmz on 2019/10/29.
+ */
+@Service
+public class SmsFlashPromotionServiceImpl implements SmsFlashPromotionService {
+    @Autowired
+    private SmsFlashPromotionMapper flashPromotionMapper;
+
+    /**
+     * 添加活动
+     */
+    @Override
+    public int create(SmsFlashPromotion flashPromotion) {
+        flashPromotion.setCreateTime(new Date());
+        return flashPromotionMapper.insert(flashPromotion);
+    }
+
+    /**
+     * 修改指定活动
+     */
+    @Override
+    public int update(Long id, SmsFlashPromotion flashPromotion) {
+        flashPromotion.setId(id);
+        return flashPromotionMapper.updateByPrimaryKey(flashPromotion);
+    }
+
+    /**
+     * 删除单个活动
+     */
+    @Override
+    public int delete(Long id) {
+        return flashPromotionMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 修改上下线状态
+     */
+    @Override
+    public int updateStatus(Long id, Integer status) {
+        SmsFlashPromotion flashPromotion = new SmsFlashPromotion();
+        flashPromotion.setId(id);
+        flashPromotion.setStatus(status);
+        return flashPromotionMapper.updateByPrimaryKeySelective(flashPromotion);
+    }
+
+    /**
+     * 获取详细信息
+     */
+    @Override
+    public SmsFlashPromotion getItem(Long id) {
+        return flashPromotionMapper.selectByPrimaryKey(id);
+    }
+
+    /**
+     * 分页查询活动
+     */
+    @Override
+    public List<SmsFlashPromotion> list(String keyword, Integer pageSize, Integer pageNum) {
+        PageHelper.startPage(pageNum, pageSize);
+        SmsFlashPromotionExample example = new SmsFlashPromotionExample();
+        if (!StringUtils.isEmpty(keyword)) {
+            example.createCriteria().andTitleLike("%" + keyword + "%");
+        }
+        return flashPromotionMapper.selectByExample(example);
+    }
+
+}
