b67a65bdcf5f5b91de8416fefbf9201f5d5bffd8
DicerZ <438840092@qq.com>
2019-10-30 10:39:52 +0800

    增加首页轮播广告管理

---
+++ src/main/java/com/zmz/malllearning/service/SmsHomeAdvertiseService.java
@@ -0,0 +1,47 @@
+package com.zmz.malllearning.service;
+
+import com.zmz.malllearning.mbg.model.SmsHomeAdvertise;
+
+import java.util.List;
+
+/**
+ * 首页广告管理Service
+ * Created by zmz on 2019/10/30.
+ */
+public interface SmsHomeAdvertiseService {
+    /**
+     * 添加广告
+     */
+    int create(SmsHomeAdvertise advertise);
+
+    /**
+     * 批量删除广告
+     */
+    int delete(List<Long> ids);
+
+    /**
+     * 修改上、下线状态
+     */
+    int updateStatus(Long id, Integer status);
+
+    /**
+     * 获取广告详情
+     */
+    SmsHomeAdvertise getItem(Long id);
+
+    /**
+     * 更新广告
+     */
+    int update(Long id, SmsHomeAdvertise advertise);
+
+    /**
+     * 分页查询广告
+     * @param name 广告名
+     * @param type 广告类型
+     * @param endTime 结束时间
+     * @param pageSize 页面长度
+     * @param pageNum 页数
+     * @return
+     */
+    List<SmsHomeAdvertise> list(String name, Integer type, String endTime, Integer pageSize, Integer pageNum);
+}
